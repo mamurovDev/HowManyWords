cb6908b2dedbf55d196081aa2c1cc40cd798ab8c
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2024-01-31 15:55:13 +0500

    set .env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,8 +7,8 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: "how-many-words-bf49e",
   storageBucket: "how-many-words-bf49e.appspot.com",
   messagingSenderId: "422576436964",
